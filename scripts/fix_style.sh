3172fc4423065d9db633bd47d754efe960d4b9ef
Abhi <suriabhinav1997@gmail.com>
2020-03-15 20:45:16 -0500

    try this?

---
+++ scripts/fix_style.sh
@@ -0,0 +1,6 @@
+# fix style
+num_errors_before=`find . -name \*.py -exec pycodestyle --ignore=e402 {} + | wc -l`
+echo $num_errors_before
+find . -name \*.py -exec autopep8 --recursive --aggressive --aggressive --in-place {} +
+num_errors_after=`find . -name \*.py -exec pycodestyle --ignore=e402 {} + | wc -l`
+echo $num_errors_after
---
+++ scripts/test.sh
@@ -0,0 +1,1 @@
+python -m unittest discover -s moore_math_beta -p 'test_*.py'
